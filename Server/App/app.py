3ec8a6e8eb5bf54bd295206dc1a8baec4e5847b8
mgn <champagneflatulence@gmail.com>
2016-10-16 17:28:51 -0700

    Can now search Yelp businesses

--- Server/App/app.py
+++ Server/App/app.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 import requests
 import json
 
-def get_yelp_token(event, context):
+def get_yelp_token():
     yelp_data = None
     with open('yelp.json') as data_file:
         yelp_data = json.load(data_file)
@@ -13,10 +13,20 @@ def get_yelp_token(event, context):
         "client_secret" : yelp_data["client_secret"]
     }
     res = requests.post("https://api.yelp.com/oauth2/token", data=request_data)
+    return res.json()
+
+def get_yelp_businesses(event, context):
+    token = get_yelp_token()
+    headers = {
+        "Authorization": token["token_type"] + " " + token["access_token"]
+    }
+
+    res = requests.get("https://api.yelp.com/v3/businesses/search", headers=headers, params=event)
+
     return {
-        'statusCode': res.status_code,
-        'body': res.json(),
-        'headers': {
-            'Content-Type': 'application/json'
+        "statusCode": res.status_code,
+        "body" : res.json(),
+        "headers" : {
+            "Content-Type" : "application/json"
         }
     }
