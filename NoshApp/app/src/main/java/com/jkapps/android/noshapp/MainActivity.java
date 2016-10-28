3d9aa12c29a3f6a0642c195614147702b12d8959
Calvin Yang <calvinyang@Calvins-MacBook.hsd1.ca.comcast.net>
2016-10-28 02:29:01 -0700

    UI to get paramters for API call

--- NoshApp/app/src/main/java/com/jkapps/android/noshapp/MainActivity.java
+++ NoshApp/app/src/main/java/com/jkapps/android/noshapp/MainActivity.java
@@ -10,8 +10,13 @@ import com.jkapps.android.noshapp.apigateway.deserializer.GetFromYelpDeserialize
 
 import java.util.Stack;
 
-public class MainActivity extends AppCompatActivity {
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Spinner;
 
+public class MainActivity extends AppCompatActivity {
     private static void testAPIGateway() {
         // we should do this with an AsyncTask
         new Thread(new Runnable() {
@@ -37,5 +42,62 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         testAPIGateway();
+
+        listenerForRatingBar();
+        listenerForDollarSpinner();
+        listenerForCategorySpinner();
     }
+
+
+    // Stuff for MainActivity UI
+    RatingBar rating;
+    TextView rating_param;
+    public void listenerForRatingBar(){
+        rating = (RatingBar) findViewById(R.id.ratingBar);
+        rating_param = (TextView) findViewById(R.id.textView);
+
+        rating.setOnRatingBarChangeListener(
+                new RatingBar.OnRatingBarChangeListener(){
+                    @Override
+                    public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                        rating_param.setText(String.valueOf(rating));
+                    }
+                }
+        );
+    }
+
+    Spinner dollars;
+    TextView dollar_param;
+    public void listenerForDollarSpinner(){
+        dollars = (Spinner) findViewById(R.id.spinner);
+        dollar_param = (TextView) findViewById(R.id.textView2);
+
+        dollars.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                dollar_param.setText(dollars.getSelectedItem().toString());
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }
+
+    Spinner categories;
+    TextView category_param;
+    public void listenerForCategorySpinner(){
+        categories = (Spinner) findViewById(R.id.spinner2);
+        category_param = (TextView) findViewById(R.id.textView3);
+
+        categories.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                category_param.setText(categories.getSelectedItem().toString());
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }
+
 }
